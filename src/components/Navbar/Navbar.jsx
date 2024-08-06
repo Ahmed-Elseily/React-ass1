674af91b21c006eb0a8157710e065c399b4a85b5
Ahmed-Elseily <accahmedkamalibrahim@gmail.com>
2024-08-06 20:16:04 +0300

    portfolio update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,9 +8,9 @@ export default function Navbar() {
 
 <nav className="bg-white border-gray-200 dark:bg-gray-700 py-6 fixed w-full z-10">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a  className="flex items-center space-x-3 rtl:space-x-reverse">
+    <span  className="flex items-center space-x-3 rtl:space-x-reverse">
       <Link  to={""} className="self-center text-4xl font-semibold whitespace-nowrap dark:text-white uppercase">Start Framework</Link>
-    </a>
+    </span>
     <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
       <span className="sr-only">Open main menu</span>
       <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,129 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import pic1 from "../../assets/pics/poert1.png"
 import pic2 from "../../assets/pics/port2.png"
 import pic3 from "../../assets/pics/port3.png"
 export default function Portfolio() {
+
+    const [modal, setModal] = useState(false)
+
+    const [selectedImg, setSelectedImg] = useState("")
+
+    const [picArray, setPicArray] = useState([
+        { img: pic1, id: 1 },
+        { img: pic2, id: 2 },
+        { img: pic3, id: 3 },
+        { img: pic1, id: 4 },
+        { img: pic2, id: 5 },
+        { img: pic3, id: 6 },
+    ])
+
+    const handleModal = (e) => {
+        if (e.target == e.currentTarget) {
+            setModal(false)
+        }
+    }
     return (
-        <div className="portfolio-section  flex flex-col justify-center items-center pt-16">
-            <div className="contact-text text-center pt-20">
-                    <h2 className='text-slate-800 uppercase text-4xl font-bold'>portfolio component</h2>
+        <>
+            <div className="portfolio-section pt-20">
+            <div className="contact-text text-center p-16">
+                    <h2 className='text-slate-800 uppercase text-4xl font-bold'>Portfolio section</h2>
                     <div className=" flex justify-center items-center mt-5 gap-3">
                         <p className='w-20 h-1 bg-slate-800'></p>
                         <i className="fa-solid fa-star star text-slate-800 text-xl"></i>
                         <p className='w-20 h-1 bg-slate-800'></p>
                     </div>
                 </div>
-            <div className="container flex flex-wrap justify-center items-center py-5">
-
-
-                {/* <!-- Modal toggle --> */}
-                
-                <div   data-modal-target="default-modal" data-modal-toggle="default-modal" className="block p-5 text-white w-1/3 rounded-xl overflow-hidden cursor-pointer" type="button">
-                    <img className='rounded-xl' src={pic1} alt="" />
+                <div className="container flex flex-wrap justify-center items-center ">
+                    {picArray.map((pic) => {
+                        return (
+                            <div key={pic.id} onClick={() => {
+                                setSelectedImg(pic.img)
+                                setModal(true)
+                            }} className="card w-1/3 p-5 ">
+                                <div className="card-inner relative ">
+                                    <img className='w-full rounded-xl' src={pic.img} alt="" />
+                                    <div className="card-cover flex justify-center items-center absolute top-0 bottom-0 left-0 right-0 hover:bg-green-500 duration-300 opacity-50 group">
+                                        <i className="fa-solid fa-plus !hidden text-white text-8xl group-hover:!block duration-500"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
-
-                {/* <!-- Main modal --> */}
-                <div id="default-modal" tabIndex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-screen max-h-full bg-blue-400 bg-opacity-50">
-                    <div className="relative p-4 w-full max-w-2xl max-h-full">
-                        {/* <!-- Modal content --> */}
-                        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                            {/* <!-- Modal header --> */}
-                            <img className='w-full' src={pic1} alt="" />
-                           
-
-                        </div>
-                    </div>
-                </div>
-                {/* <!-- Modal toggle --> */}
-                
-                <div  data-modal-target="default-modal2" data-modal-toggle="default-modal2" className="block p-5 text-white w-1/3 rounded-xl overflow-hidden cursor-pointer" type="button">
-                    <img className='rounded-xl' src={pic2} alt="" />
-                </div>
-
-                {/* <!-- Main modal --> */}
-                <div id="default-modal2" tabIndex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-screen max-h-full bg-blue-400 bg-opacity-50">
-                    <div className="relative p-4 w-full max-w-2xl max-h-full">
-                        {/* <!-- Modal content --> */}
-                        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                            {/* <!-- Modal header --> */}
-                            <img className='w-full' src={pic2} alt="" />
-                           
-
-                        </div>
-                    </div>
-                </div>
-                {/* <!-- Modal toggle --> */}
-                
-                <div  data-modal-target="default-modal3" data-modal-toggle="default-modal3" className="block p-5 text-white w-1/3 rounded-xl overflow-hidden cursor-pointer" type="button">
-                    <img className='rounded-xl' src={pic3} alt="" />
-                </div>
-
-                {/* <!-- Main modal --> */}
-                <div id="default-modal3" tabIndex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-screen max-h-full bg-blue-400 bg-opacity-50">
-                    <div className="relative p-4 w-full max-w-2xl max-h-full">
-                        {/* <!-- Modal content --> */}
-                        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                            {/* <!-- Modal header --> */}
-                            <img className='w-full' src={pic3} alt="" />
-                           
-
-                        </div>
-                    </div>
-                </div>
-                <div   data-modal-target="default-modal" data-modal-toggle="default-modal" className="block p-5 text-white w-1/3 rounded-xl overflow-hidden cursor-pointer" type="button">
-                    <img className='rounded-xl' src={pic1} alt="" />
-                </div>
-
-                {/* <!-- Main modal --> */}
-                <div id="default-modal" tabIndex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-screen max-h-full bg-blue-400 bg-opacity-50">
-                    <div className="relative p-4 w-full max-w-2xl max-h-full">
-                        {/* <!-- Modal content --> */}
-                        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                            {/* <!-- Modal header --> */}
-                            <img className='w-full' src={pic1} alt="" />
-                           
-
-                        </div>
-                    </div>
-                </div>
-                {/* <!-- Modal toggle --> */}
-                
-                <div  data-modal-target="default-modal2" data-modal-toggle="default-modal2" className="block p-5 text-white w-1/3 rounded-xl overflow-hidden cursor-pointer" type="button">
-                    <img className='rounded-xl' src={pic2} alt="" />
-                </div>
-
-                {/* <!-- Main modal --> */}
-                <div id="default-modal2" tabIndex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-screen max-h-full bg-blue-400 bg-opacity-50">
-                    <div className="relative p-4 w-full max-w-2xl max-h-full">
-                        {/* <!-- Modal content --> */}
-                        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                            {/* <!-- Modal header --> */}
-                            <img className='w-full' src={pic2} alt="" />
-                           
-
-                        </div>
-                    </div>
-                </div>
-                {/* <!-- Modal toggle --> */}
-                
-                <div  data-modal-target="default-modal3" data-modal-toggle="default-modal3" className="block p-5 text-white w-1/3 rounded-xl overflow-hidden cursor-pointer" type="button">
-                    <img className='rounded-xl' src={pic3} alt="" />
-                </div>
-
-                {/* <!-- Main modal --> */}
-                <div id="default-modal3" tabIndex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-screen max-h-full bg-blue-400 bg-opacity-50">
-                    <div className="relative p-4 w-full max-w-2xl max-h-full">
-                        {/* <!-- Modal content --> */}
-                        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                            {/* <!-- Modal header --> */}
-                            <img className='w-full' src={pic3} alt="" />
-                           
-
-                        </div>
-                    </div>
-                </div>
-
             </div>
-        </div>
+            <div onClick={handleModal} className={`${modal ? "block" : "hidden"} big-layer fixed z-20 top-0 bottom-0 left-0  right-0 bg-blue-500 bg-opacity-50 flex justify-center items-center`}>
+                <img width={600} src={selectedImg} alt="" />
+            </div>
+
+        </>
     )
 }
